7c781f52ed3080edbbcefbc9694b41c86754bf9c
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 17:10:30 +0530

    Size of different types example

--- lesson_1/main.cpp
+++ lesson_1/main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout<<"Hello, World!"<<endl;
+
+    //cout example
+    int integer = 43;
+    cout<<"The value of integer is: "<<integer<<endl;
+    cout<<"The size of integer is: "<<sizeof(int)<<endl;
+    cout<<"The size of float is: "<<sizeof(float)<<endl;
+    cout<<"The size of short is: "<<sizeof(short)<<endl;
+    cout<<"The size of character is: "<<sizeof(char)<<endl;
     return 0;
 }
