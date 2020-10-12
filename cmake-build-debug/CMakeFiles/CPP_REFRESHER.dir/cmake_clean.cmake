1e875cbd0cd7b7af7b4334d5f5f03bd338d89efe
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 16:46:20 +0530

    First Commit

---
+++ cmake-build-debug/CMakeFiles/CPP_REFRESHER.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_REFRESHER.dir/main.cpp.o"
+  "CPP_REFRESHER"
+  "CPP_REFRESHER.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_REFRESHER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
