403134fb26cc224b5601fda2f8005832dbcea2f2
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2020-10-12 17:03:18 +0530

    Namespace example

---
+++ lesson_1/cmake-build-debug/CMakeFiles/lesson_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson_1.dir/main.cpp.o"
+  "lesson_1"
+  "lesson_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lesson_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
